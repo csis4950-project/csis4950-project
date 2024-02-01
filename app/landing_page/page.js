611a7bda18c5eac586b921904d0f3d4d222cde44
kjustowebdev <karlojusto@gmail.com>
2024-01-31 17:59:07 -0800

    added navbar

---
+++ app/landing_page/page.js
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar";
+
+
+export default function landingpage() {
+    return (
+    <body>
+        <Navbar />
+        <section>
+            <div>
+                <h1>Work at the speed of thought</h1>
+                <p>Most calendars are designed for teams. Slate is designed for  freelancers who want a simple way to plan their schedule.</p>
+            </div>
+        </section>
+    </body>
+);
+}
