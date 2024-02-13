2824d6b9168123a24716472b7d9952366c6ac4e8
Shingo Tennichi <sngsng.ttt@gmail.com>
2024-02-13 00:26:33 -0800

    add code for redirect based on session

--- middleware.js
+++ middleware.js
@@ -1,10 +1,25 @@
 import { NextResponse } from 'next/server';
-
-export default function middleware(request) {
-  // console.log(request);
-  // console.log("middleware");
-}
+import { getSession, updateSession } from '@/utils/utils';
 
 export const config = {
-  matcher: '/',
+  matcher: ["/user/:path*", "/"]
+}
+
+export default async function middleware(request) {
+  const path = request.nextUrl.pathname;
+  const session = await getSession();
+  if (path === "/") {
+    if (session) {
+      const response = updateSession(session);
+      return response.redirect(new URL("/user/dashboard", request.url));
+    }
+  }
+
+  if (path.includes("user")) {
+    if (!session) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+
+    return updateSession(session);
+  }
 }
