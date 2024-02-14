cb07cbed395acc9460de6356e300c9c209a12a23
Shingo Tennichi <sngsng.ttt@gmail.com>
2024-02-13 16:29:08 -0800

    add demo sign up page

--- app/(user_auth)/sign-up/page.jsx
+++ app/(user_auth)/sign-up/page.jsx
@@ -1,7 +1,20 @@
+import { signUp } from '@/utils/actions';
+import { redirect, permanentRedirect } from "next/navigation";
+import { cookies } from 'next/headers';
+import Input from './Input';
+
+
 export default function SignUp() {
+  const c = cookies().get('session');
+  if (c) {
+    redirect("/user/dashboard", "replace");
+  }
   return (
-    <div>
-      <h1>Sign Up here</h1>
-    </div>
+    <form action={async (formData) => {
+      'use server';
+      await signUp(formData);
+    }}>
+      <Input />
+    </form>
   )
 }
