9b860c569887ec34bd4053ed90aeb4b05787a6b3
Shingo Tennichi <sngsng.ttt@gmail.com>
2024-02-13 19:00:30 -0800

    update login page ui

--- app/(user_auth)/login/page.jsx
+++ app/(user_auth)/login/page.jsx
@@ -1,63 +1,74 @@
 import Image from "next/image";
+import Link from "next/link";
+
 export default function Login() {
   return (
-    <main className="login">
+    <div className="login-page">
+      <div className="login-img">
+        <Image src="/login_img.png"
+          alt="login image"
+          width={100}
+          height={100}
+          sizes="100vw"
+          style={{
+            width: '100%',
+            height: '100%',
+            objectFit: "cover"
+          }}>
+        </Image>
+      </div>
       <section>
-        <div className="container">
-          <div className="loginpic">
-          <Image src= "/loginBackground.png"
-              width={100}
-              height={100}
-              sizes="100vw"
-              style={{
-                width: '100%',
-                height: 'auto',
-              }}>
-            </Image>
-          </div>
-            <div className="loginForm">
-              <div className="formLogo">
-                   <Image src="/wehabu.png"
-                      alt="main_image1"
-                      width={80}
-                      height={80}
-                      style={{ objectFit: "contain" }}
-                    />
-                         <span>Wehabu</span>
-                </div>
-              <form className="loginBox">
-                <div className="usernameBox">
-                  <label for="username">Email:</label>
-                  <input type="text" id="Email" name="Email" required></input>
-                  <Image src="/img.png"
-                    className="usernameImage"
-                      alt="main_image1"
-                      width={22}
-                      height={22}
-                      style={{ objectFit: "contain" }}
-                    />
-                </div>
-                <div className="passwordBox">
-                  <label for="username">Password:</label>
-                  <input type="text" id="password" name="password" required></input>
-                  <Image src="/img.png"
-                    className="passwordImage"
-                      alt="main_image1"
-                      width={22}
-                      height={22}
-                      style={{ objectFit: "contain" }}
-                    />
-                </div>
-              </form>
-
+        <div className="login-form">
+          <div className="logo-frame">
+            <div className="logo">
+              <Image src="/wehabu.png"
+                alt="logo"
+                width={80}
+                height={80}
+                style={{ objectFit: "contain" }}
+              />
+              <span>Wehabu</span>
             </div>
-
-
-
+            <div className="message">
+              <span >Login into your account</span>
+            </div>
+          </div>
+          <form action={async () => {
+            "use server";
+            console.log('action');
+          }}>
+            <div className="input-field">
+              <div className="mb-12">
+                <label htmlFor="email">Email</label>
+              </div>
+              <div className="input">
+                <input type="email" id="email" name="email" required></input>
+              </div>
+            </div>
+            <div className="input-field">
+              <div className="mb-12">
+                <label htmlFor="password">password</label>
+              </div>
+              <div className="input">
+                <input type="password" id="password" name="password" required></input>
+              </div>
+            </div>
+            <div className="flex flex--right mb-24">
+              <Link className="link link__forget-password link--black-500" href="/">Forgot password?</Link>
+            </div>
+            <button className="btn" type="submit"><span>Login</span></button>
+          </form>
         </div>
-
+        <div className="divider">
+          <div className="horizontal-line"></div>
+          <span>OR</span>
+          <span className="horizontal-line"></span>
+        </div>
+        <button className="btn btn--transparent">
+          <Link className="link link--blue-500" href="/sign-up">Signup now</Link>
+        </button>
       </section>
-    </main>
+    </div>
   )
 }
 
