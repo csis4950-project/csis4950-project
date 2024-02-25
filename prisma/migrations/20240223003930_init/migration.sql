448b97175a5ccf05a7c19b185d7aa46ca81fd762
Shingo Tennichi <sngsng.ttt@gmail.com>
2024-02-24 20:06:50 -0800

    update database tables

--- prisma/migrations/20240223003930_init/migration.sql
+++ prisma/migrations/20240223003930_init/migration.sql
@@ -40,7 +40,7 @@ CREATE TABLE "Department" (
 CREATE TABLE "Role" (
     "id" STRING NOT NULL,
     "name" STRING(25) NOT NULL,
-    "description" STRING(100) NOT NULL,
+    "description" STRING(150) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "deletedAt" TIMESTAMP(3),
@@ -61,6 +61,10 @@ CREATE TABLE "DepartmentMember" (
 -- CreateTable
 CREATE TABLE "Request" (
     "id" STRING NOT NULL,
+    "ownerId" STRING NOT NULL,
+    "departmentId" STRING NOT NULL,
+    "typeTagId" STRING NOT NULL,
+    "statusTagId" STRING NOT NULL,
     "detail" STRING(50) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
@@ -87,6 +91,9 @@ CREATE TABLE "WorkTime" (
     "id" STRING NOT NULL,
     "date" TIMESTAMP(3) NOT NULL,
     "hoursWorked" INT4 NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
 
     CONSTRAINT "WorkTime_pkey" PRIMARY KEY ("id")
 );
@@ -97,6 +104,9 @@ CREATE TABLE "Availability" (
     "date" TIMESTAMP(3) NOT NULL,
     "day" TIMESTAMP(3) NOT NULL,
     "time" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
 
     CONSTRAINT "Availability_pkey" PRIMARY KEY ("id")
 );
@@ -104,8 +114,15 @@ CREATE TABLE "Availability" (
 -- CreateTable
 CREATE TABLE "Announcement" (
     "id" STRING NOT NULL,
+    "ownerId" STRING NOT NULL,
+    "departmentId" STRING NOT NULL,
+    "typeTagId" STRING NOT NULL,
     "title" STRING(25) NOT NULL,
     "detail" STRING(100) NOT NULL,
+    "expirationTime" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
 
     CONSTRAINT "Announcement_pkey" PRIMARY KEY ("id")
 );
@@ -113,10 +130,25 @@ CREATE TABLE "Announcement" (
 -- CreateTable
 CREATE TABLE "Tag" (
     "id" STRING NOT NULL,
+    "tagTypeId" STRING NOT NULL,
     "name" STRING(25) NOT NULL,
-    "description" STRING(50) NOT NULL,
+    "description" STRING(100) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
 
     CONSTRAINT "Tag_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TagType" (
+    "id" STRING NOT NULL,
+    "name" STRING(25) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "TagType_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -139,3 +171,24 @@ ALTER TABLE "DepartmentMember" ADD CONSTRAINT "DepartmentMember_memberId_fkey" F
 
 -- AddForeignKey
 ALTER TABLE "DepartmentMember" ADD CONSTRAINT "DepartmentMember_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Request" ADD CONSTRAINT "Request_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Request" ADD CONSTRAINT "Request_typeTagId_fkey" FOREIGN KEY ("typeTagId") REFERENCES "Tag"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Request" ADD CONSTRAINT "Request_statusTagId_fkey" FOREIGN KEY ("statusTagId") REFERENCES "Tag"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Announcement" ADD CONSTRAINT "Announcement_departmentId_fkey" FOREIGN KEY ("departmentId") REFERENCES "Department"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Announcement" ADD CONSTRAINT "Announcement_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Announcement" ADD CONSTRAINT "Announcement_typeTagId_fkey" FOREIGN KEY ("typeTagId") REFERENCES "Tag"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Tag" ADD CONSTRAINT "Tag_tagTypeId_fkey" FOREIGN KEY ("tagTypeId") REFERENCES "TagType"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
